ecad671cb7626195018d5a19cdb8b54098005231
Zak.Estrada@gmail.com <Zak.Estrada@gmail.com@bd93647f-e9fc-3ba5-bb13-5f1b9cddd14f>
2012-04-27 06:18:41 +0000

    Latest template for common classes

--- Code/common/TSPCommon.java
+++ Code/common/TSPCommon.java
@@ -5,7 +5,8 @@
 public class TSPCommon {
   public static class InputOutput{
     public surface readData(String filename) {
-
+      //Need to read in coordinates from file and create a cell
+      surface mySurf = new surface(xcoords, ycoords, cell);
     }
   }
 
@@ -15,19 +16,51 @@ public class TSPCommon {
  */
   public class surface {
 
-    //Array of 2 element double arrays to store our points
-    private double[][] points; 
+    //Distance matrix
+    private double[][] distance; 
     //Array to count number of connections each site currently has
-    private int[] verticies;
-    //Number of sites
-    private int N;
+    private int[] vertices;
+    //Number of sites, vertices per site
+    private int N,maxVertex;
+    //Connection matrix
+    private boolean[][] connection;
+    //Simulation cell
+    private cell cell;
 
 
     /*
      * Base constructor
      */
-    public surface(double[] xcoords, double[] ycoords) {
-      this.N = xcoords.length;
+    public surface(double[] xcoords, double[] ycoords, int nvertex, cell cell) {
+      this.N = xcoords.length; //assums xcoords.length==ycoords.length
+      this.cell = cell;
+      this.maxVertex = nvertex; 
+
+      //Allocate matrices
+      this.vertices = new int[N];
+      this.connection = new boolean[N][N];
+
+      //Now, compute distance matrix
+      for(int i=0; i<N;i++) {
+        vertices[i]=0; 
+        for(int j=0; j<N; j++) {
+          connection[i][j] = false;
+        }
+      }
+    }
+  }
+
+  public class cell {
+
+    //Boundaries
+    private double xmin,xmax;
+    private double ymin,ymax;
+
+    public cell(double xmin, double xmax, double ymin, double ymax) {
+      this.xmin = xmin;
+      this.xmax = xmax;
+      this.ymin = ymin;
+      this.ymax = ymax;
     }
   }
 }
