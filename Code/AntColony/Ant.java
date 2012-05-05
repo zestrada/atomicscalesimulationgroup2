beca275eca17a4612c106bf4abc87548b91d550d
Zak.Estrada <Zak.Estrada@bd93647f-e9fc-3ba5-bb13-5f1b9cddd14f>
2012-05-05 06:56:17 +0000

    More work on AntColony, small changes to surface

--- Code/AntColony/Ant.java
+++ Code/AntColony/Ant.java
@@ -2,14 +2,22 @@ public class Ant {
   private Surface surf;
   private double[] pheromone;
 
-  public Ant(Surface inputSurf) throws java.lang.CloneNotSupportedException {
-    int N;
+  public Ant(Surface inputSurf, double[] inputPheromone) throws java.lang.CloneNotSupportedException {
     surf = inputSurf.clone(); 
-    N = surf.getN();
+    pheromone = inputPheromone;
   }
 
   //Reset this ant's state
   public void reset() {
     surf.disconnectAll();
+  }
+
+  public void setPheromone(double[] newPheromone) {
+    pheromone = newPheromone;
+  }
+
+  //Do the main work based on Surface, pheromone
+  public void constructSolution() {
+
   }
 }
