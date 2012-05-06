28f423d9ab8ab96242fb678afe096306add0ffcd
qq151069 <qq151069@bd93647f-e9fc-3ba5-bb13-5f1b9cddd14f>
2012-05-06 06:50:49 +0000

    GWTW stuff

---
+++ trunk/Code/GoWithTheWinners/GWTWmain.java
@@ -0,0 +1,15 @@
+public class GWTWmain {
+  
+    private static Surface surface;
+
+  public static void main(String[] args) {
+      gwtwInit();
+
+      System.out.print("DONE\n");
+  }
+
+  public static void gwtwInit() {
+	surface = new Surface();
+  }
+
+}
