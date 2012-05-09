01456e4ba7c7c53c2d704abe83f3698cdef7b05e
qq151069 <qq151069@bd93647f-e9fc-3ba5-bb13-5f1b9cddd14f>
2012-05-09 05:01:37 +0000

    Random adds

--- trunk/Code/GoWithTheWinners/GWTWmain.java
+++ trunk/Code/GoWithTheWinners/GWTWmain.java
@@ -1,15 +1,20 @@
 public class GWTWmain {
   
     private static Surface surface;
+    private static GWTW[] g = new GWTW[5];
 
   public static void main(String[] args) {
-      gwtwInit();
-
+      //gwtwInit();
+      for(int i = 0; i < 5; i++) {
+	  g[i] = new GWTW(i);
+	  g[i].setID(i);
+	  g[i].start();
+      }
       System.out.print("DONE\n");
   }
 
   public static void gwtwInit() {
-	surface = new Surface();
+      //surface = new Surface();
   }
 
 }
