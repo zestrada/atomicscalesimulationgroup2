d9b7f58162d1bc728262cfc5a56f19212848f4bb
qq151069 <qq151069@bd93647f-e9fc-3ba5-bb13-5f1b9cddd14f>
2012-05-09 02:48:54 +0000

    Updated SA code

--- Code/SA/SAmain.java
+++ Code/SA/SAmain.java
@@ -8,6 +8,8 @@ public class SAmain {
 
     public static void main(String[] args) {
 	simulatedAnnealing = new SA();
-    }
-    
+	simulatedAnnealing.run(1000000,10000);
+	simulatedAnnealing.run(1000000,10000);
+	simulatedAnnealing.finalOutput();
+    }    
 }
