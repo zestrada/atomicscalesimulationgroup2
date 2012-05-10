0d643d8bf4249b703ff8284004b0d1af5e9714f3
chandinijain206 <chandinijain206@bd93647f-e9fc-3ba5-bb13-5f1b9cddd14f>
2012-05-10 19:01:32 +0000

    added GA

---
+++ Code/GA_copy/GApopulation.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class GApopulation { 
+
+    private int ID;
+    private Surface surface;
+    private int N;
+    private static Random rng = new Random();
+    
+    public GApopulation(int ID) {
+	this.ID = ID;
+	GAInit();
+    }
+    
+    public double getEnergy() {
+        return surface.getEnergy();
+    }
+    
+    public Surface getSurface() {
+        return surface;
+    }
+    
+    public void setSurface(Surface s) {
+        surface = s1;
+    }
+    
+    public int getN() {
+        return N;
+    }
+    
+    private void GAInit() {
+        TSPInOut tsp = new TSPInOut();
+        surface = tsp.readData("../GA_copy/square.input");
+        N = surface.getN();
+        //System.out.println("Initializing simulation");
+        //System.out.println("There are: " + N + " particles in simulation");
+        preProcessor();
+    }
+    
+    private void GAInit( Surface s) {
+        TSPInOut tsp = new TSPInOut();
+        surface = s;
+        N = surface.getN();
+        //System.out.println("Initializing simulation");
+        //System.out.println("There are: " + N + " particles in simulation");
+        //preProcessor();
+    }
+    
+    private void preProcessor() {
+        int[] distIndex;
+        for(int i = 0; i < N; i++) {
+            distIndex = surface.getShortestDistance(i);
+            int a = rng.nextInt(3);
+            int tmp = distIndex.length+1;
+            int i1 = tmp;
+            for(int j = 0; j < a; j++) {
+                //System.out.println("Connecting: " + i + " & " + distIndex[j] + " " + surface.getDist(i,distIndex[j]));
+                while (i1==tmp){
+                    i1 = rng.nextInt(distIndex.length);
+                }
+                surface.connectUnsafe(i,distIndex[i1]);
+                tmp = i1;
+            } 
+        }
+    }
+    
+}
