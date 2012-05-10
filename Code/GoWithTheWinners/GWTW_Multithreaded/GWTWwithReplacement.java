34e305eaa4d14cad709610dc33359f56051e3477
qq151069 <qq151069@bd93647f-e9fc-3ba5-bb13-5f1b9cddd14f>
2012-05-10 06:49:33 +0000

    Wow...  I modified I lot of shit.

---
+++ Code/GoWithTheWinners/GWTW_Multithreaded/GWTWwithReplacement.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class GWTWwithReplacement extends Thread { 
+
+    private int ID;
+    private SA simulatedAnnealing;
+    private int steps;
+    private int output;
+
+    public GWTWwithReplacement(int steps, int output, int ID, SA simulatedAnnealing) {
+	this.ID = ID;
+	this.steps = steps;
+	this.output = output;
+	this.simulatedAnnealing = simulatedAnnealing;
+    }
+
+    public void run() {	
+	simulatedAnnealing.runSilentWithReplacement(steps,output);
+    }
+
+}
