792529920586fea24c286ec6dd4f24fde05832b0
qq151069 <qq151069@bd93647f-e9fc-3ba5-bb13-5f1b9cddd14f>
2012-05-10 07:14:20 +0000

    Removed useless files

--- Code/SA/SAmain.java
+++ Code/SA/SAmain.java
@@ -7,9 +7,9 @@ public class SAmain {
     private static SA simulatedAnnealing;
 
     public static void main(String[] args) {
-	simulatedAnnealing = new SA("../common/test.input",50);
-	simulatedAnnealing.finalOutput();
-	simulatedAnnealing.setTemperature(100);
+	simulatedAnnealing = new SA("../common/sheared.input",50);
+	//simulatedAnnealing.finalOutput();
+	simulatedAnnealing.setTemperature(5);
 	for(int i = 0; i < 10; i++) {
 	    simulatedAnnealing.runWithReplacement(100000,100000);
 	    simulatedAnnealing.run(100000,100000);
