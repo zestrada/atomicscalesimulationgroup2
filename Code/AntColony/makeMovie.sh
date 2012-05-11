122e74a98ac886e53cb4bd4725e18f408f9c527e
Zak.Estrada <Zak.Estrada@bd93647f-e9fc-3ba5-bb13-5f1b9cddd14f>
2012-05-11 09:31:10 +0000

    Added movie ... used convert -density 100 -delay 8 output?.ps output??.ps output???.ps movie.mpg

--- Code/AntColony/makeMovie.sh
+++ Code/AntColony/makeMovie.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for i in `seq 0 9`
+for i in `seq 0 230`
 do
-    sed -e "s/XXX/$i/g" < view.tcl > tmp.tcl
-    vmd surface.xyz < tmp.tcl
+    sed -e "s/XXX/$i/g" < view.tcl > tmp1.tcl
+    vmd surface.xyz < tmp1.tcl
 done
