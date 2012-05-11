3a4e4afd7b4fa3d16aaf6e5bfab145a2a9612740
chandinijain206 <chandinijain206@bd93647f-e9fc-3ba5-bb13-5f1b9cddd14f>
2012-05-11 11:07:26 +0000

    Presentation  updated

--- Code/GA_copy/GApopulation.java
+++ Code/GA_copy/GApopulation.java
@@ -32,7 +32,7 @@ public class GApopulation {
     private void GAInit() {
         TSPInOut tsp = new TSPInOut();
         tsp.disableStdout();
-        surface = tsp.readData("../GA_copy/squareRandom.input");
+        surface = tsp.readData("../GA_copy/sheared2.input");
         N = surface.getN();
         //System.out.println("Initializing simulation");
         //System.out.println("There are: " + N + " particles in simulation");
