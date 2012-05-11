f0be92ae305809a45ed5020d75c437820c959606
qq151069 <qq151069@bd93647f-e9fc-3ba5-bb13-5f1b9cddd14f>
2012-05-11 02:31:48 +0000

    AX

--- Code/SA/SAmain.java
+++ Code/SA/SAmain.java
@@ -7,12 +7,12 @@ public class SAmain {
     private static SA simulatedAnnealing;
 
     public static void main(String[] args) {
-	simulatedAnnealing = new SA("../common/sheared.input",50,0);
+	simulatedAnnealing = new SA("../common/squareRandom.input",50,0);
 	//simulatedAnnealing.finalOutput();
 	simulatedAnnealing.setTemperature(5);
 	for(int i = 0; i < 10; i++) {
-	    simulatedAnnealing.runWithReplacement(500000,100000);
-	    simulatedAnnealing.run(500000,100000);
+	    simulatedAnnealing.runWithReplacement(50000,10000);
+	    simulatedAnnealing.run(50000,10000);
 	}
 	simulatedAnnealing.finalOutput();
     }    
